2d704ac8428b933e64a94fed8c18341b0a805067
vibhuti-jadav <vibhutij109@gmail.com>
2025-08-28 16:05:31 +0530

    module

---
+++ 02-modules/greet.js
@@ -0,0 +1,11 @@
+
+export function greet(){
+    return "greet"
+}
+
+export function farewell(){
+    return "farewell"
+}
+
+// module.exports = {greet ,farewell}
+
---
+++ 02-modules/index.js
@@ -0,0 +1,6 @@
+// const {greet ,farewell} = require("./greet");
+
+import { greet , farewell } from "./greet.js";
+
+console.log(greet());
+console.log(farewell());
